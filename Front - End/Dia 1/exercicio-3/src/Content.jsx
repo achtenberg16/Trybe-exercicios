196f323fb2c4dcde8678a9297444d0d7e768baba
achtenberg167 <achtenberg16@gmail.com.br>
2022-01-22 13:27:03 -0300

    adiionando exercicios

---
+++ Front - End/Dia 1/exercicio-3/src/Content.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+const conteudos = [
+    {
+      conteudo: 'High Order Functions',
+      bloco: 8,
+      status: 'Aprendido'
+    },
+    {
+      conteudo: 'Composicao de Componentes',
+      bloco: 11,
+      status: 'Aprendendo',
+    },
+    {
+      conteudo: 'Composicao de Estados',
+      bloco: 12,
+      status: 'Aprenderei'
+    },
+    {
+      conteudo: 'Redux',
+      bloco: 16,
+      status: 'Aprenderei'
+    },
+  ];
+
+function formatContent ({conteudo, bloco, status}) {
+ return <p>O conteúdo é: {conteudo}
+  Status: {status}
+  Bloco: {bloco}</p>
+}
+
+  class Content extends React.Component {
+      render(){
+       return(<div>
+       {conteudos.map((element) => formatContent(element))}
+       </div>)
+      }
+  }
+
+  export default Content
