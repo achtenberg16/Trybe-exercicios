b1fc65433d4d70548aef723cef02d1c9c2a673e3
achtenberg16 <achtenberg16@gmail.com>
2022-04-20 17:02:08 -0300

    add new querie

--- Back End/SQL - dia 2/Bloco20Dia2Bonus.sql
+++ Back End/SQL - dia 2/Bloco20Dia2Bonus.sql
@@ -48,3 +48,6 @@ INSERT INTO Provides(Piece, Provider, Price)
 -- Escreva uma query para exibir a peça e o preço de tudo que é provido pela empresa RBT .
 
 SELECT Price, Piece FROM `PiecesProviders`.`Provides` WHERE Provider = 'RBT';
+
+-- Escreve uma query para exibir todas as informações das cinco peças com os maiores preços.
+SELECT * FROM `PiecesProviders`.`Provides` ORDER BY Price DESC LIMIT 5;
